47b36b957920f081a9dabeb36b500a1dd4597ab0
Geovany Nunes Gonçalves <geovanynunes7007@gmail.com>
2025-04-28 15:43:39 -0300

    adicionando novas paginas para as receitas

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,3 +1,9 @@
-function verReceita() {
+function verReceita1() {
     window.location.href = "hamburguer.html"
+}
+function verReceita2() {
+    window.location.href = "bife.html"
+}
+function verReceita3() {
+    window.location.href = "ovo.html"
 }
